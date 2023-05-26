49962a3612afe2134254c1dc487178e727c5cbf7
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-05-26 14:32:43 +0900

    계산기 기능 구현

--- calculatorAndNotepad/panel/calculator.Designer.cs
+++ calculatorAndNotepad/panel/calculator.Designer.cs
@@ -34,7 +34,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.resultText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -75,6 +75,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(150, 63);
             this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -96,19 +97,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox3
+            // resultText
             // 
-            this.textBox3.Font = new System.Drawing.Font("D2Coding", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox3.Location = new System.Drawing.Point(235, 512);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(166, 69);
-            this.textBox3.TabIndex = 9;
+            this.resultText.Font = new System.Drawing.Font("D2Coding", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.resultText.Location = new System.Drawing.Point(182, 511);
+            this.resultText.Name = "resultText";
+            this.resultText.ReadOnly = true;
+            this.resultText.Size = new System.Drawing.Size(281, 69);
+            this.resultText.TabIndex = 9;
+            this.resultText.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("D2Coding", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(141, 530);
+            this.label1.Location = new System.Drawing.Point(81, 529);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 43);
             this.label1.TabIndex = 10;
@@ -124,6 +127,7 @@
             this.button2.TabIndex = 12;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -134,6 +138,7 @@
             this.button3.TabIndex = 12;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -144,6 +149,7 @@
             this.button4.TabIndex = 12;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label2
             // 
@@ -200,7 +206,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.resultText);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -221,7 +227,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox resultText;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- calculatorAndNotepad/panel/calculator.cs
+++ calculatorAndNotepad/panel/calculator.cs
@@ -24,22 +24,54 @@ namespace calculatorAndNotepad.panel
             OnPictureClick?.Invoke(this, EventArgs.Empty);
         }
 
+        // 값1
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // 값2
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // + 연산
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultText.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text));
+        }
+
+        // - 연산
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultText.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) - Convert.ToDouble(textBox2.Text));
+        }
+
+        // * 연산
+        private void button3_Click(object sender, EventArgs e)
+        {
+            resultText.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text));
+        }
+
+        // / 연산
+        private void button4_Click(object sender, EventArgs e)
+        {
+            resultText.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) / Convert.ToDouble(textBox2.Text));
+        }
+
+        // 결과 텍스트 박스
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
         private void lblCalc_Click(object sender, EventArgs e)
         {
 
         }
 
         private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
